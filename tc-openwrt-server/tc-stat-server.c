eb69b9743fafe9d6c67d93cbc2ffab64c8c6ed6c
Vamseedhar <vamseedhar.reddyvari@gmail.com>
2015-05-01 14:52:48 -0500

    server runs continuously

--- tc-openwrt-server/tc-stat-server.c
+++ tc-openwrt-server/tc-stat-server.c
@@ -1,9 +1,11 @@
 /* A server for transmitting output of tc command to client */
-#include<stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
 
 // All communication happens on this port
 const char *SERVER_PORT_NO = "9000";
@@ -28,47 +30,53 @@ int main(int argc, char *argv[])
   struct addrinfo host_info;// Filled by getaddrinfo()
   struct addrinfo *host_info_list;
   memset(&host_info, 0, sizeof host_info); // getaddrinfo() expects all zeros
-  //cout<<"Initialization Done"<<endl;
+  printf("Initialization Done\n");
   host_info.ai_family = AF_UNSPEC;     // IP version not specified. Can be both.
   host_info.ai_socktype = SOCK_STREAM; // Use SOCK_STREAM for TCP or SOCK_DGRAM for UDP.
   host_info.ai_flags = AI_PASSIVE;     // IP Wildcard
   status = getaddrinfo(NULL,SERVER_PORT_NO, &host_info, &host_info_list);
-  //if (status != 0)  //cout << "getaddrinfo error" << gai_strerror(status) ;
+  if (status != 0)  printf("getaddrinfo error\n");
 
-  //cout << "Creating a socket..."  << std::endl;
+  printf("Creating a socket...\n");
   int socketfd ; // The socket descripter
-  socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype,
-                        host_info_list->ai_protocol);
-  //if (socketfd == -1)  std:://cout << "socket error " ;
+  socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype, host_info_list->ai_protocol);
+  if (socketfd == -1)  printf("socket error\n") ;
 
-  //cout << "Binding a socket..."  << std::endl;
+  printf("Binding a socket...\n");
   int yes = 1;
   status = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
   status = bind(socketfd, host_info_list->ai_addr, host_info_list->ai_addrlen);
   if (status == -1)  printf("bind error\n");
 
-  printf("Listen()ing for connections...\n");
-  status =  listen(socketfd, 5);
-  if (status == -1)  printf("Listen error\n");
+  while(1){
+    printf("Listen()ing for connections...\n");
+    status =  listen(socketfd, 5);
+    if (status == -1)  printf("Listen error\n");
 
-  int new_fd; // The socket descripter for client
-  struct sockaddr_storage their_addr;
-  socklen_t addr_size = sizeof(their_addr);
-  new_fd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
-  printf("Accepted Connection...\n");
+    int new_fd; // The socket descripter for client
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof(their_addr);
+    new_fd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
+    printf("Accepted Connection...\n");
 
-  printf("send()ing back a message...\n");
-  char msg[1000] = "";// = "The Messave from tc server.";
-  command_output("ls",msg);
+    // Need to create a thrd or Forking a child process to communicate
 
-  int len;
-  ssize_t bytes_sent;
-  len = strlen(msg);
-  bytes_sent = send(new_fd, msg, len, 0);
 
+    printf("send()ing back a message...\n");
+    char msg[1000] = "";// = "The Messave from tc server.";
+    command_output("ls",msg);
+
+    int len;
+    ssize_t bytes_sent;
+    len = strlen(msg);
+    bytes_sent = send(new_fd, msg, len, 0);
+    //sleep(10);
+
+    // Need to send a termination message
+   close(new_fd);
+  }
   //cout << "Stopping server..." << std::endl;
   freeaddrinfo(host_info_list);
   //close(new_fd);
-  //close(socketfd);
   return 0;
 }
